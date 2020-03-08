3686d586ba79a2ae40f727f3610bc786106e71c4
J. Oluwaseyi Afolayan <afolayanseyi@gmail.com>
2020-03-08 14:07:48 +0100

    checking something

--- RaspPiCamera.py
+++ RaspPiCamera.py
@@ -79,7 +79,7 @@ class CameraOptions(object):
     isVideo = False
     camera = None
     # capture_time = get_image_capture_time()
-    capture_time = 1
+    capture_time = 0.3
     timestamp = datetime.now().strftime('%d-%m-%y_%H-%M-%S')
 
     def __init__(self):
@@ -120,70 +120,18 @@ class CameraOptions(object):
             self.stop_capture()
             return "Cannot complete this process."
 
-    # def single_video_capture(self):
-    #     print("single_video_capture")
-    #     self.init_camera()
-    #     filename = 'my_video.h264'
-    #     print(" started now at {}".format(self.timestamp))
-    #     self.camera.start_preview()
-    #     self.camera.start_recording(filename)
-    #     sleep(30)
-    #     self.camera.stop_recording()
-    #     self.camera.stop_preview()
-    #     print(" ended now at {}".format(self.timestamp))
-    #     upload(filename, False)
-
     def single_video_capture(self):
-        # Connect a client socket to my_server:8000 (change my_server to the
-        # hostname of your server)
-        client_socket = socket.socket()
-        post_image_stream_url = 'https://deepbuzz-project.azurewebsites.net/api/ImageUpload/PostImageStream'
-        # client_socket.connect((post_image_stream_url, 8000))
-
-        # Make a file-like object out of the connection
-        # connection = client_socket.makefile('wb')
-        try:
-            # Start a preview and let the camera warm up for 2 seconds
-            self.init_camera()
-            self.camera.start_preview()
-            time.sleep(2)
-
-            # Note the start time and construct a stream to hold image data
-            # temporarily (we could write it directly to connection but in this
-            # case we want to find out the size of each capture first to keep
-            # our protocol simple)
-            start = time.time()
-            stream = io.BytesIO()
-            for foo in self.camera.capture_continuous(stream, 'png'):
-                # Write the length of the capture to the stream and flush to
-                # ensure it actually gets sent
-                # connection.write(struct.pack('<L', stream.tell()))
-                # connection.flush()
-                # Rewind the stream and send the image data over the wire
-                stream.seek(0)
-                datum = {'byteArray': stream.read()}
-                tryAndVerifyImage(foo)
-                tryAndVerifyImage(stream)
-
-                print(stream.read())
-
-                res = requests.post(url=post_image_stream_url,
-                                    json=datum,
-                                    headers={'Content-Type': 'application/octet-stream'}
-                                    )
-                print("res us ", res)
-                # connection.write(stream.read())
-                # If we've been capturing for more than 30 seconds, quit
-                if time.time() - start > 30:
-                    break
-                # Reset the stream for the next capture
-                stream.seek(0)
-                stream.truncate()
-            # Write a length of zero to the stream to signal we're done
-            # connection.write(struct.pack('<L', 0))
-        finally:
-            # connection.close()
-            client_socket.close()
+        print("single_video_capture")
+        self.init_camera()
+        filename = 'my_video.h264'
+        print(" started now at {}".format(self.timestamp))
+        self.camera.start_preview()
+        self.camera.start_recording(filename)
+        sleep(30)
+        self.camera.stop_recording()
+        self.camera.stop_preview()
+        print(" ended now at {}".format(self.timestamp))
+        upload(filename, False)
 
     def multiple_video_capture(self, count):
         print("multiple_video_capture")
