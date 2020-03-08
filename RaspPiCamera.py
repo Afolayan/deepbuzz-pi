9f37cbd1b3f8efb4c574c7f1045b91c01a2c3fd5
J. Oluwaseyi Afolayan <afolayanseyi@gmail.com>
2020-03-08 12:30:28 +0100

    checking something

--- RaspPiCamera.py
+++ RaspPiCamera.py
@@ -165,7 +165,7 @@ class CameraOptions(object):
                 tryAndVerifyImage(stream)
                 datum = {'byteArray': stream}
                 res = requests.post(url=post_image_stream_url,
-                                    data={'byteArray': stream.read()},
+                                    data=stream.read(),
                                     headers={'Content-Type': 'application/octet-stream'}
                                     )
                 print("res us ", res)
